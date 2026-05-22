da396495e9f72518de1534b135e6cf2f8b26d145
Rebecca D. Martins <rebeccamartins.d@gmail.com>
2026-05-21 21:37:29 -0300

    remove prisma completely

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,26 +1,5 @@
-
-
-/*import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-// GET - listar produtos
 export async function GET() {
-    const products = await prisma.product.findMany();
-
-    return NextResponse.json(products);
+    return NextResponse.json([]);
 }
-
-// POST - criar produto
-export async function POST(request: Request) {
-    const body = await request.json();
-
-    const newProduct = await prisma.product.create({
-        data: {
-            title: body.title,
-            description: body.description,
-            category: body.category,
-        },
-    });
-
-    return NextResponse.json(newProduct);
-}*/
--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,7 @@
 export default function Home() {
   return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+    <main>
       <h1>ReUse</h1>
-
-      <p>
-        Plataforma colaborativa para troca de produtos e incentivo ao consumo consciente.
-      </p>
-
-      <h2>Funcionalidades</h2>
-
-      <ul>
-        <li>Cadastro de produtos</li>
-        <li>Integração com chatbot</li>
-        <li>Automação com Watson Assistant</li>
-        <li>Integração com Supabase</li>
-      </ul>
     </main>
   );
 }
-
-/*import { prisma } from "@/lib/prisma";
-
-export default async function Home() {
-  const products = await prisma.product.findMany();
-
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif"}}>
-      <h1 className="text-3xl font-bold mb-3" >
-        ReUse! 
-      </h1>
-      <h2 className="text-2xl font-medium mb-6">Plataforma de Troca de Itens</h2>
-      <p className="mb-8 text-gray-600">
-        Cadastre itens que você não usa mais e encontre oportunidades de troca.
-      </p>
-
-      <a href="/cadastrar-produto">Cadastrar novo produto</a>
-
-      <div
-        style={{
-          marginTop: "30px",
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          gap: "20px",
-        }}
-      >
-        {products.map((product) => (
-          <a
-            key={product.id}
-            href={`/produtos/${product.id}`}
-            style={{
-              border: "2px solid #a8a8a8",
-              borderRadius: "10px",
-              padding: "20px",
-              textDecoration: "none",
-              color: "black",
-              background: "white",
-            }}
-          >
-            <h3>{product.title}</h3>
-
-            <p style={{ fontSize: "14px", opacity: 0.7 }}>
-              {product.category}
-            </p>
-
-            <p style={{ marginTop: "10px" }}>
-              {product.description.slice(0, 60)}...
-            </p>
-          </a>
-        ))}
-      </div>
-    </main>
-  );
-}*/
