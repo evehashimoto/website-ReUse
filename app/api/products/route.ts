28a04bdb3e8a54e9494035ada662efa0fefe411f
Rebecca D. Martins <rebeccamartins.d@gmail.com>
2026-05-26 22:25:02 -0300

    teste de rota de api

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,22 +1,5 @@
-export const runtime = "nodejs";
-
-import { NextResponse } from "next/server";
-
-const products = [
-    {
-        id: 1,
-        title: "Notebook Dell",
-        description: "Notebook usado em ótimo estado.",
-        category: "Tecnologia",
-    },
-    {
-        id: 2,
-        title: "Mesa Gamer",
-        description: "Mesa compacta para escritório.",
-        category: "Móveis",
-    },
-];
-
 export async function GET() {
-    return NextResponse.json(products);
+    return Response.json({
+        status: "API funcionando 🚀"
+    });
 }
